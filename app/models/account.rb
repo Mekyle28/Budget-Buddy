fa5132e471fd78951a6d74aead127992848f23b4
creynolds8 <cameron8.cr@gmail.com>
2024-07-09 14:30:43 -0700

    add error handling to accounts form

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,10 @@
 class Account < ApplicationRecord
   has_many :transactions, dependent: :destroy
 
-  validates :current_balance_cents, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
+  validates :account_type, presence: true
+  validates :current_balance, numericality: { greater_than: 0}
+  validates :current_balance_cents, numericality: { greater_than: 0 }
 
   monetize :current_balance_cents, as: :current_balance
 end
