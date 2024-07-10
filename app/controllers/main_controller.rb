fcc1c2cb718b2967534eaa59d2309263ae4b4634
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-07-09 20:30:01 -0400

    start implementing the total budget progress bar

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,12 @@
 class MainController < ApplicationController
   def index
     @budget_categories = Budget.includes(:category).where(current_month: true)
+  
+    @total_fact = @budget_categories.map do |category|
+      category.fact_amount_cents
+    end
+    puts "total fact************************ #{@total_fact}*****************"
+  
   end
 
 end
